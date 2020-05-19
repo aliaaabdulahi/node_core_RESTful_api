8d8feafa6ae93a335806725e01bcf500c352e108
Alia Ali <39501230+aliamuraayad@users.noreply.github.com>
2020-05-19 17:26:45 -0400

    Update server.js

--- nodehttp/server.js
+++ nodehttp/server.js
@@ -1,10 +1,12 @@
 // this isn't project, this is just to learn HTTP 
 //we're gonna use the core HTTP module instead of express which is lower level
 // express gives us a whole bunch of utilities and methods that make things easier
-// using this raw HTTP module will give us the understanding of the request/response cycle and header / content type etc
+// using this raw HTTP module will give us the understanding of the request/response cycle 
+//and header / content type etc
 
 const http = require('http');
-// this is, ofc, a core node module, it comes with node so we don't have to install it with npm. if we wanted to install a third party module like express, we'd have to use npm
+// this is, ofc, a core node module, it comes with node so we don't have to install it with npm. 
+//if we wanted to install a third party module like express, we'd have to use npm
 
 //mock data for API
 const todos = [
@@ -16,7 +18,8 @@ const todos = [
 const server = http.createServer((req, res) =>{
 //this method allows us to create servers that take requests and send back responses
 // res.statusCode = 200; // how to manually send a status code (right now its automatic)
-// res.setHeader('Content-Type', 'application/json'); // first param is the header key(content-type), then key (which is text/plain)
+// res.setHeader('Content-Type', 'application/json'); // first param is the header key(content-type), 
+//then key (which is text/plain)
 // res.setHeader('X-Powered-By', 'Node.js');
 
 const { method, url } = req;
@@ -79,12 +82,19 @@ req.on('data', chunk => {
     //     success:false, //set this false to indicate that its a 404
     //     data: null,
     //     error: 'Not Found' //null bc not found
-    //     // the data coming back will be the todos. The parameter of res.end is an object, JSON.stringify is what is going to turn it into a string so it can be parsed as one
-    // })); // to get a correct response back. ends teh response wihtout getting any data. as long as there is no error will always be a 200 response. when we make a request and get the data value back. when we use express we don't have to do stringify, or put the headers, but using node modules lets you know how things happen under the hood
+    //     // the data coming back will be the todos. The parameter of res.end is an object, JSON.stringify 
+           // is what is going to turn it into a string so it can be parsed as one
+    // })); // to get a correct response back. ends teh response wihtout getting any data. as long
+            // as there is no error will always be a 200 response. when we make a request and get
+            // the data value back. when we use express we don't have to do stringify, or put the headers, 
+            // but using node modules lets you know how things happen under the hood
 
 
-    // we need to move what we need to in this method bc this is where we actually have access to the body data 
-}) // you have to listen to the request and the request is a readable stream of data and events. rquest on certain events, so on data, we take in chunk parameter, we push chunk onto body & on event 'end' , we have access to buffer, which lets us concat to the body
+    // we need to move what we need to in this method bc this is where we actually have access to the 
+    //body data 
+}) // you have to listen to the request and the request is a readable stream of data and events. 
+    // rquest on certain events, so on data, we take in chunk parameter, we push chunk onto body &
+    // on event 'end' , we have access to buffer, which lets us concat to the body
 
 });
 
@@ -93,4 +103,5 @@ const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 //in order to call our server, we jall the listen method on it which listens for a specific port
 //typically we will have a variable (usually in the config file, but here we'll just set it to 5000)
-//on the listen method, we put the port as a parameter, but we can also put down an optional parameter of function console.log a message saying the port is running
+//on the listen method, we put the port as a parameter, but we can also put down an optional parameter 
+// of function console.log a message saying the port is running
